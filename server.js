4d051275ca1df2ace5d7614cf64c9a80545c31a0
Vikram S <vikram.senthil@outlook.com>
2020-11-27 14:37:40 +0530

    Added port number

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
