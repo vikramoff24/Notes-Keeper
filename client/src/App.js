ec5bc931928544f19964af1654fa820c8b856a34
Vikram S <vikram.senthil@outlook.com>
2020-12-03 22:41:07 +0530

    Added options in navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
+import React, { Fragment } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar.js";
-
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import { BrowserRouter as Switch, Route, Router } from "react-router-dom";
 function App() {
-  return <Navbar />;
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ title, icon }) => {
   return (
-    <div className="navbar bg-primary">
-      <h1>
+    <div className="navbar bg-primary ">
+      <h1 className="header">
         <i className={icon} /> {title}
       </h1>
-      <ul>
-        <li></li>
-      </ul>
+      <Fragment>
+        <ul>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </Fragment>
     </div>
   );
 };
-
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string,
