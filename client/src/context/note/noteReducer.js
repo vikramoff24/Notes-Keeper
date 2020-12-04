87132208d01d1593715fd3e541ba694318fc4432
Vikram S <vikram.senthil@outlook.com>
2020-12-04 13:00:29 +0530

    Update noteReducer.js

---
+++ client/src/context/note/noteReducer.js
@@ -0,0 +1,35 @@
+import {
+  GET_NOTES,
+  ADD_NOTE,
+  DELETE_NOTE,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  CLEAR_NOTES,
+  UPDATE_NOTE,
+  FILTER_NOTES,
+  CLEAR_FILTER,
+  SET_ALERT,
+  REMOVE_ALERT,
+  NOTE_ERROR,
+} from "../types";
+
+export default (state, action) => {
+  switch (action.type) {
+    case GET_NOTES:
+      return {
+        ...state,
+      };
+    case ADD_NOTE:
+      return {
+        ...state,
+        notes: [action.payload, ...state.notes],
+      };
+    case DELETE_NOTE:
+      return {
+        ...state,
+        notes: notes.filter((note) => note._id != action.payload),
+      };
+    default:
+      return state;
+  }
+};
