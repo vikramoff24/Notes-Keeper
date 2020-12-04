94187e1ebec4e892da82d76161bb0b714e60ce4d
Vikram S <vikram.senthil@outlook.com>
2020-12-04 10:42:22 +0530

    created noteContext

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
         <Navbar />
         <div className="container">
           <Switch>
+            {/* <Route exact path="/" component={Home} /> */}
             <Route exact path="/register" component={Register} />
 
             <Route exact path="/login" component={Login} />
