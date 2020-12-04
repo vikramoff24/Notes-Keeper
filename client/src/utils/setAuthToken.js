22a0e2e56c9dfd5e1563a29027db6418725e80be
Vikram S <vikram.senthil@outlook.com>
2020-12-05 00:49:40 +0530

    Create setAuthToken.js

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,12 @@
+//setting the passed token to global header.
+import axios from "axios";
+
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common["x-auth-token"] = token;
+  } else {
+    delete axios.defaults.headers.common["x-auth-token"];
+  }
+};
+
+export default setAuthToken;
