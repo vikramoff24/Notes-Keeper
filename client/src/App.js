55029c312b6ca14fc463eb3687ae7136641e1da4
Vikram S <vikram.senthil@outlook.com>
2020-12-05 13:30:37 +0530

    alerts created

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Home from "./components/pages/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NoteState from "./context/note/NoteState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+import Alerts from "./components/layout/Alerts";
 import setAuthToken from "./utils/setAuthToken";
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -15,20 +17,23 @@ function App() {
   return (
     <AuthState>
       <NoteState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
 
-                <Route exact path="/register" component={Register} />
+                  <Route exact path="/register" component={Register} />
 
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </NoteState>
     </AuthState>
   );
