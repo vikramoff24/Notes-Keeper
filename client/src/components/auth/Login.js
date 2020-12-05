eb7990925bbc4ec5dd4ddec1b005b29e5c5e8607
Vikram S <vikram.senthil@outlook.com>
2020-12-05 23:29:58 +0530

    Update Login.js

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AuthContext from "../../context/auth/authContext";
+import AlertContext from "../../context/alert/alertContext";
 
 const Login = () => {
+  const authContext = useContext(AuthContext);
+  const alertContext = useContext(AlertContext);
+  const { register, isAuthenticated, error, clearErrors } = authContext;
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     email: "",
     password: "",
@@ -10,12 +17,20 @@ const Login = () => {
   const onChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (email === "" || password === "") {
+      setAlert("Please fill in all fields", "danger");
+    } else {
+      login({ email, password });
+    }
+  };
   return (
     <div className="form-container">
       <h1>
         Account <span className="text-primary">Login</span>
       </h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
           <input type="email" name="email" value={email} onChange={onChange} />
