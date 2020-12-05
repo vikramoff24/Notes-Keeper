01f0557e3182f1438a9c3656fd051ba372f1ca80
Vikram S <vikram.senthil@outlook.com>
2020-12-05 12:40:01 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,24 +6,31 @@ import Login from "./components/auth/Login";
 import Home from "./components/pages/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NoteState from "./context/note/NoteState";
+import AuthState from "./context/auth/AuthState";
+import setAuthToken from "./utils/setAuthToken";
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 function App() {
   return (
-    <NoteState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
+    <AuthState>
+      <NoteState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
 
-              <Route exact path="/register" component={Register} />
+                <Route exact path="/register" component={Register} />
 
-              <Route exact path="/login" component={Login} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </NoteState>
+                <Route exact path="/login" component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </NoteState>
+    </AuthState>
   );
 }
 
