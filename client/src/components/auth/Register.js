f0308a38cc0288a816e3fdf53dac018e6775d29e
Vikram S <vikram.senthil@outlook.com>
2020-12-05 23:29:48 +0530

    Error fixed

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -42,53 +42,49 @@ const Register = (props) => {
     } else {
       register({ name, email, password });
     }
-    return (
-      <div className="form-container">
-        <h1>
-          Account <span className="text-primary"> Register</span>
-        </h1>
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" value={name} onChange={onChange} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email Address</label>
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={onChange}
-              minLength="6"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password2">Confirm Password</label>
-            <input
-              type="password"
-              name="password2"
-              value={password2}
-              onChange={onChange}
-              minLength="6"
-            />
-          </div>
-          <input
-            type="submit"
-            value="Register"
-            className="btn btn-primary btn-block"
-          />
-        </form>
-      </div>
-    );
   };
+  return (
+    <div className="form-container">
+      <h1>
+        Account <span className="text-primary"> Register</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" value={name} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password2">Confirm Password</label>
+          <input
+            type="password"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+            minLength="6"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Register"
+          className="btn btn-primary btn-block"
+        />
+      </form>
+    </div>
+  );
 };
+
 export default Register;
