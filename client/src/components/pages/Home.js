09c6715eda37016437aea80984dbccbb7ecc9542
Vikram S <vikram.senthil@outlook.com>
2020-12-06 22:15:44 +0530

    Update Home.js

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import NoteForm from "../notes/NoteForm";
 import Notes from "../notes/Notes";
+import AuthContext from "../../context/auth/authContext";
 const Home = () => {
+  const authContext = useContext(AuthContext);
+  useEffect(() => {
+    authContext.loadUser();
+    // eslint-disable-next-line
+  }, []);
   return (
     <Fragment>
       <NoteForm />
-      <Notes />
+      <div className="note-section">
+        <Notes />
+      </div>
     </Fragment>
   );
 };
