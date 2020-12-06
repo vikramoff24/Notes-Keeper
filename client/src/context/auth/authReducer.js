6f0d3fb5d20b325b3b00a05d13411a558b446444
Vikram S <vikram.senthil@outlook.com>
2020-12-06 22:15:38 +0530

    Update authReducer.js

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -44,7 +44,7 @@ export default (state, action) => {
         ...state,
         token: null,
         isAuthenticated: false,
-        loading: true,
+        loading: false,
         user: null,
         error: action.payload,
       };
