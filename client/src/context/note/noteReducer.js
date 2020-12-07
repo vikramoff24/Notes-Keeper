de9d9a46fee3b28020c478a0cd214bfbb192bb78
Vikram S <vikram.senthil@outlook.com>
2020-12-07 12:21:31 +0530

    Update noteReducer.js

--- client/src/context/note/noteReducer.js
+++ client/src/context/note/noteReducer.js
@@ -18,22 +18,31 @@ export default (state, action) => {
     case GET_NOTES:
       return {
         ...state,
+        notes: action.payload,
+        loading: false,
       };
     case ADD_NOTE:
       return {
         ...state,
         notes: [action.payload, ...state.notes],
+        loading: false,
       };
     case DELETE_NOTE:
       return {
         ...state,
         notes: state.notes.filter((note) => note._id != action.payload),
+        loading: false,
       };
     case CLEAR_NOTES:
       return {
         ...state,
-        notes: null,
+        notes: [],
         error: null,
+      };
+    case NOTE_ERROR:
+      return {
+        ...state,
+        error: action.payload,
       };
     default:
       return state;
