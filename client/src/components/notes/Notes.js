6491195b7870b2a61ea0c1832ee3de1549cd90ee
Vikram S <vikram.senthil@outlook.com>
2020-12-07 12:21:24 +0530

    Update Notes.js

--- client/src/components/notes/Notes.js
+++ client/src/components/notes/Notes.js
@@ -1,9 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import NoteItem from "./NoteItem";
 import NoteContext from "../../context/note/noteContext";
 const Notes = () => {
   const noteContext = useContext(NoteContext);
-  const { notes } = noteContext;
+  const { getNotes, notes } = noteContext;
+
+  useEffect(() => {
+    getNotes();
+    //eslint-disable-next-line
+  }, []);
+
   return notes.map((note) => (
     <NoteItem
       _id={note._id}
